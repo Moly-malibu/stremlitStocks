617b7aed88601273f58b18a716c616953a6ef8bb
Monica Bustamante <58006376+Moly-malibu@users.noreply.github.com>
2022-02-20 12:03:09 -0800

    update librarys

---
+++ home.py
@@ -0,0 +1,15 @@
+#base
+import streamlit as st
+import streamlit.components.v1 as components
+import sqlite3
+import pandas as pd
+
+#Index
+from numpy.lib.shape_base import column_stack
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from datetime import datetime
+import datetime
+import requests
+from requests import get
+from bs4 import BeautifulSoup
